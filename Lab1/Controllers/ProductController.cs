c7307958ddcbdeb3d90e22633dfcf54c398e5bf4
Fir3AtWill <Fir3AtWill@DESKTOP-94C6DB6>
2017-09-25 21:14:43 -0400

    First Commit Lab 1 project

---
+++ Lab1/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string product)
+        {
+            ViewBag.Product = "You selected product: " + product;
+            return View();
+        }
+    }
+}
